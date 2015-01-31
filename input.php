0945ae5f93893b313ab914855741fbf47e944f1c
Arthur Atrokhov <aatrokhov@gmail.com>
2015-02-01 00:08:41 +0600

    fixed indentation

--- input.php
+++ input.php
@@ -1,11 +1,11 @@
 <html>
 <body>
-<form action="form.php" method="POST">
-User Name: <input name="user" type="text" />
-Email: <input name="email" type="text" />
-Password: <input name="password" type="password" />
-New password: <input name="newPassword" type="password" />
-<input type="submit" />
-</form>
+  <form action="form.php" method="POST">
+    User Name: <input name="user" type="text" />
+    Email: <input name="email" type="text" />
+    Password: <input name="password" type="password" />
+    New password: <input name="newPassword" type="password" />
+    <input type="submit" />
+  </form>
 </body>
 </html>
