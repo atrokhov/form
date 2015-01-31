7884b78a624a055f865a6c93bff1d76d8dc506c5
Arthur Atrokhov <aatrokhov@gmail.com>
2015-02-01 01:15:52 +0600

    Added some html formatting

--- form.php
+++ form.php
@@ -2,10 +2,10 @@
 $user=$_POST['user'];
 echo "Hello $user";
 $email=$_POST['email'];
-echo "Your email is: $email";
+echo "Your email is: $email\n";
 $password=$_POST['password'];
-echo "Your password is: $password";
+echo "Your password is: $password\n";
 $newPassword=$_POST['newPassword'];
-echo "Your new password is: $newPassword";
+echo "Your new password is: $newPassword\n";
 echo "Thank you.";
 ?>
--- input.php
+++ input.php
@@ -1,11 +1,20 @@
 <html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 <body>
-  <form action="form.php" method="POST">
-    User Name: <input name="user" type="text" />
-    Email: <input name="email" type="text" />
-    Password: <input name="password" type="password" />
-    New password: <input name="newPassword" type="password" />
-    <input type="submit" />
+  <div class="container-fluid col-md-6 col-md-offset-3" >
+  <form action="form.php" method="POST" class="form-horizontal">
+    <div class="form-group">
+    <label for="i1">User Name: </label>
+    <input name="user" type="text" id="i1" class="form-control"/>
+    <label for="i2">Email: </label>
+    <input name="email" type="text" id="i2" class="form-control"/>
+    <label for="i3">Password: </label>
+    <input name="password" type="password" id="i3" class="form-control"/>
+    <label for="i4">New password:</label>
+    <input name="newPassword" type="password" id="i4" class="form-control"/>
+    <input  type="submit"/>
+  </div>
   </form>
+  <div>
 </body>
 </html>
